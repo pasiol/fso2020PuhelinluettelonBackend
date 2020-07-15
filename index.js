1405b189a0fb15489738685db4c0efce0cf79b89
Pasi Ollikainen <pasi.ollikainen@outlook.com>
2020-07-15 08:52:31 +0300

    heroku

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const morgan = require("morgan")
+const morgan = require('morgan')
+const cors =  require('cors')
 const app = express()
 
 morgan.token("data", function getData(res) {
@@ -10,6 +11,7 @@ morgan.token("data", function getData(res) {
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 app.set("json spaces", 4);
+app.use(cors())
 
 let persons = [
     { 
@@ -79,8 +81,7 @@ app.post('/api/persons', (request, response) => {
     }
 })
 
-
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
